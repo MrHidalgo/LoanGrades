c621e4fa041643d12670dabba2ad39193cb2f6dc
Andrii <Luckyhidalgo@gmail.com>
2020-06-10 18:20:38 +0300

    Table - added description toggle, lowest sort.

--- dest/js/app.js
+++ dest/js/app.js
@@ -37,18 +37,89 @@ var initPreventBehavior = function initPreventBehavior() {
 	/*
  * CALLBACK :: start
  * ============================================= */
+	var btnToggle = function btnToggle() {
+		$('[btn-toggle-js]').on('click', function (ev) {
+			$('[btn-toggle-js]').removeClass('is-active');
+			$(ev.currentTarget).addClass('is-active');
+		});
+		$('[link-toggle-js]').on('click', function (ev) {
+			$('[link-toggle-js]').removeClass('is-active');
+			$(ev.currentTarget).addClass('is-active');
+		});
+	};
+
+	var toggleTableRow = function toggleTableRow() {
+		$('[table-toggle-js]').on('click', function (ev) {
+			var el = $(ev.currentTarget),
+			    elID = el.attr('data-id');
+
+			if (el.hasClass('is-open')) {
+				el.removeClass('is-open');
+				el.siblings('.table__block-wrapper[data-id="' + elID + '"]').slideUp(250);
+			} else {
+				$('[table-toggle-js]').removeClass('is-open');
+				$('.table__block-wrapper').slideUp(250);
+
+				el.addClass('is-open');
+				el.siblings('.table__block-wrapper[data-id="' + elID + '"]').slideDown(250);
+			}
+		});
+	};
+
 	var searchTable = function searchTable() {
-		var $rows = $('#table .table__tbody .table__tr');
+		var _rows = $('#table .table__tbody .table__tr');
 
 		$('#search').keyup(function (ev) {
 			var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
 
-			$rows.show().filter(function () {
+			_rows.show().filter(function () {
 				var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
 
 				return !~text.indexOf(val);
 			}).hide();
 		});
+	};
+
+	var sortNumeric = function sortNumeric() {
+		var sortNumberTable1 = function sortNumberTable1() {
+			var table = void 0,
+			    rows = void 0,
+			    switching = void 0,
+			    i = void 0,
+			    x = void 0,
+			    y = void 0,
+			    shouldSwitch = void 0;
+
+			table = document.getElementById("table");
+			switching = true;
+
+			while (switching) {
+				switching = false;
+				rows = table.querySelectorAll('.table__tbody .table__tr');
+
+				for (i = 1; i < rows.length - 1; i++) {
+					shouldSwitch = false;
+
+					x = rows[i].querySelector(".table__td:last-of-type");
+					y = rows[i + 1].querySelector(".table__td:last-of-type");
+
+					if (Number(x.innerHTML) > Number(y.innerHTML)) {
+						shouldSwitch = true;
+						break;
+					}
+				}
+
+				if (shouldSwitch) {
+					rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+					switching = true;
+				}
+			}
+		};
+
+		$('[sort-lowest-js]').on('click', function (ev) {
+			sortNumberTable1();
+		});
+		$('[sort-highest-js]').on('click', function (ev) {});
 	};
 	/*
  * CALLBACK :: end
@@ -69,6 +140,9 @@ var initPreventBehavior = function initPreventBehavior() {
 
 		// callback
 		searchTable();
+		btnToggle();
+		sortNumeric();
+		toggleTableRow();
 		// ==========================================
 	};
 	initNative();
--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -5,18 +5,88 @@
 	/*
 	* CALLBACK :: start
 	* ============================================= */
+	const btnToggle = () => {
+		$('[btn-toggle-js]').on('click', (ev) => {
+			$('[btn-toggle-js]').removeClass('is-active');
+			$(ev.currentTarget).addClass('is-active');
+		});
+		$('[link-toggle-js]').on('click', (ev) => {
+			$('[link-toggle-js]').removeClass('is-active');
+			$(ev.currentTarget).addClass('is-active');
+		});
+	};
+
+
+	const toggleTableRow = () => {
+		$('[table-toggle-js]').on('click', (ev) => {
+			const el = $(ev.currentTarget),
+				elID = el.attr('data-id');
+
+			if(el.hasClass('is-open')) {
+				el.removeClass('is-open');
+				el.siblings('.table__block-wrapper[data-id="' + elID + '"]').slideUp(250);
+			} else {
+				$('[table-toggle-js]').removeClass('is-open');
+				$('.table__block-wrapper').slideUp(250);
+
+				el.addClass('is-open');
+				el.siblings('.table__block-wrapper[data-id="' + elID + '"]').slideDown(250);
+			}
+
+		});
+	};
+
+
 	const searchTable = () => {
-		let $rows = $('#table .table__tbody .table__tr');
+		let _rows = $('#table .table__tbody .table__tr');
 
 		$('#search').keyup(function(ev) {
 			let val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
 
-			$rows.show().filter(function() {
+			_rows.show().filter(function() {
 				let text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
 
 				return !~text.indexOf(val);
 			}).hide();
 		});
+	};
+
+
+	const sortNumeric = () => {
+		const sortNumberTable1 = () => {
+			let table, rows, switching, i, x, y, shouldSwitch;
+
+			table = document.getElementById("table");
+			switching = true;
+
+			while (switching) {
+				switching = false;
+				rows = table.querySelectorAll('.table__tbody .table__tr');
+
+				for (i = 1; i < (rows.length - 1); i++) {
+					shouldSwitch = false;
+
+					x = rows[i].querySelector(".table__td:last-of-type");
+					y = rows[i + 1].querySelector(".table__td:last-of-type");
+
+					if (Number(x.innerHTML) > Number(y.innerHTML)) {
+						shouldSwitch = true;
+						break;
+					}
+				}
+
+				if (shouldSwitch) {
+					rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+					switching = true;
+				}
+			}
+		};
+
+
+		$('[sort-lowest-js]').on('click', (ev) => {
+			sortNumberTable1();
+		});
+		$('[sort-highest-js]').on('click', (ev) => {});
 	};
 	/*
 	* CALLBACK :: end
@@ -38,6 +108,9 @@
 
 		// callback
 		searchTable();
+		btnToggle();
+		sortNumeric();
+		toggleTableRow();
 		// ==========================================
 	};
 	initNative();
